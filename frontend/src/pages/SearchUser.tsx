b8160adf30e0f75f012b3b0623ca4841072dabe0
rbhagat32 <raghavbhagat32@gmail.com>
2024-11-09 17:59:07 +0530

    back to home clear state

--- frontend/src/pages/SearchUser.tsx
+++ frontend/src/pages/SearchUser.tsx
@@ -1,14 +1,32 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import SearchBox from "../components/SearchBox";
+import { useDispatch } from "react-redux";
+import { setActiveChat } from "../store/reducers/MiscSlice";
 
 const SearchUser = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const goToHome = () => {
+    dispatch(
+      setActiveChat({
+        chatId: "",
+        userId: "",
+        username: "",
+        avatar: "",
+        bio: "",
+      })
+    );
+    navigate("/");
+  };
+
   return (
     <>
       <div className="w-full px-3 py-3 flex justify-between">
-        <Link to={"/"} className="text-3xl font-bold">
+        <button onClick={goToHome} className="text-3xl font-bold">
           Echo
           <span className="inline-block ml-0.5 size-1.5 bg-indigo-400 rounded-full"></span>
-        </Link>
+        </button>
       </div>
 
       <div className="flex flex-col items-center">
