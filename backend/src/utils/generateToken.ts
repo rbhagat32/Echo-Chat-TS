27ffa77671c3dbe4c64e5cf87b0349b9d8c78f03
rbhagat32 <raghavbhagat32@gmail.com>
2025-04-15 13:18:03 +0530

    before frontend migration

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -8,13 +8,9 @@ interface User {
 
 const generateToken = (res: Response, user: User) => {
   const token = jwt.sign(
-    {
-      userId: user._id,
-    },
+    { userId: user._id },
     process.env.JWT_SECRET_KEY as string,
-    {
-      expiresIn: "15d",
-    }
+    { expiresIn: "15d" }
   );
 
   res.cookie("token", token, cookieOptions);
