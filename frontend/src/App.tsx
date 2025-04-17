9ce25355fa93e0327345844a807a9e06e350d2f1
rbhagat32 <raghavbhagat32@gmail.com>
2025-04-17 22:06:31 +0530

    tailwind + shadcn installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,3 @@
-import { Toaster } from "sonner";
-import PageLoader from "./partials/PageLoader";
-import { useCheckLoginQuery } from "./store/api";
-import Routing from "./utils/Routing";
-
-const App = () => {
-  const { isLoading, data } = useCheckLoginQuery();
-
-  return isLoading ? (
-    <PageLoader />
-  ) : (
-    <>
-      <Toaster richColors position="top-center" duration={1500} />
-      <Routing isLoggedIn={data?.isLoggedIn!} />
-    </>
-  );
-};
-
-export default App;
+export default function App() {
+  return <div>App</div>;
+}
