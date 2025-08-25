f8c171d55670143466164bbedb5149dbc68fb122
rbhagat32 <raghavbhagat32@gmail.com>
2025-08-26 01:46:54 +0530

    stop open image url on click

--- frontend/src/components/custom/ImageWithSkeleton.tsx
+++ frontend/src/components/custom/ImageWithSkeleton.tsx
@@ -13,14 +13,14 @@ export const ImageWithSkeleton = ({
 
   return (
     <div className="relative flex max-h-64 w-full items-center justify-center">
-      {!loaded && <div className="h-64 w-80 animate-pulse rounded-md bg-zinc-700" />}
+      {!loaded && <div className="h-64 w-96 animate-pulse rounded-md bg-zinc-700" />}
       <img
         src={src}
         alt={alt}
-        className={`max-h-64 cursor-pointer rounded-md object-contain transition-opacity duration-300 ${
+        className={`max-h-64 rounded-md object-contain transition-opacity duration-300 ${
           loaded ? "opacity-100" : "absolute opacity-0"
         }`}
-        onClick={() => window.open(src, "_blank")}
+        // onClick={() => window.open(src, "_blank")}
         onLoad={() => {
           setLoaded(true);
           onLoadCallback?.();
